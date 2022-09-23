968d9fe0dd6d0b3c02dc5e15a545536f6522324c
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-23 16:32:46 -0300

    test-3

--- src/app.js
+++ src/app.js
@@ -7,16 +7,17 @@ const ticketRoute = require('./routes/ticketRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
+app.use(cors({Origin: '*', Header: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', Methods: 'GET, PUT, POST, DELETE, TRUNCATE'}))
+app.use('/', index)
+app.use('/ticket', ticketRoute)
+
+module.exports = app
+
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     if (req.method === 'OPTIONS'){
       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
     }
     next()
-})
-// router.use(cors({Origin: '*', Header: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', Methods: 'GET, PUT, POST, DELETE, TRUNCATE'}))
-app.use('/', index)
-app.use('/ticket', ticketRoute)
-
-module.exports = app
+})*/
