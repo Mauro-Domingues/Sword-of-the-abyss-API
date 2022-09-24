4d0844a44dbf64408c79e84b4d2b809a77b036ce
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-24 11:27:11 -0300

    cors-moved

--- src/app.js
+++ src/app.js
@@ -6,13 +6,6 @@ const ticketRoute = require('./routes/ticketRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, content-type, Accept, Authorization')
-    if (req.method === 'OPTIONS'){
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
-    }next()
-})
 app.use('/', index)
 app.use('/ticket', ticketRoute)
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
+  }next()
+})
+
 router.get("/", (req, res, next) => {
   try{
     res.status(200).send("Conexão com o Banco de Dados estabelecida")
