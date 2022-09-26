c8a7951a7832d1b41391b161833f0d787121fbc5
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 11:42:54 -0300

    cors-test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://mauro-domingues.github.io/Sword-of-the-abyss, https://mauro-domingues.github.io/Sword-of-the-abyss-admin')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   if (req.method === 'OPTIONS'){
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
