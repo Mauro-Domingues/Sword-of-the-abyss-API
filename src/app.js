8d69c15725c3c37297f8f501033bacabb92d89e9
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 20:34:38 -0300

    silly-correction

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const ticketRoute = require('./routes/ticketRoute')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-  req.header({'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'})
+  res.header({'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'})
   if (req.method === 'OPTIONS'){
     res.header({'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, TRUNCATE'})
   }next()
