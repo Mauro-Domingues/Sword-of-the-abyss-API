0d65f3db5d30c6cb4105d2aac793c70bc0f75c07
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 12:13:58 -0300

    cors-3

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,17 @@ const express = require("express")
 const cors = require("cors")
 const router = express.Router()
 
+router.use(cors((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
+  }
+  next()
+}))
+
 router.get("/", (req, res, next) => {
   try{
-    router.use(cors(() => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-      if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
-      }
-    }))
     res.status(200).send("Conexão com o Banco de Dados estabelecida")
   }catch(error){
     res.status(400).send({
