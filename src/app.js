c9a0634ce699bf513e6d1d2c8871f346f668b057
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 20:40:21 -0300

    cors-add

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const index = require('./routes/index')
 const ticketRoute = require('./routes/ticketRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-  res.header({'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'})
-  if (req.method === 'OPTIONS'){
-    res.header({'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, TRUNCATE'})
-  }next()
-})
+app.use(cors({
+  origin: '*',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  methods: 'GET, PUT, POST, DELETE, TRUNCATE'
+}))
 app.use('/', index)
 app.use('/ticket', ticketRoute)
 
