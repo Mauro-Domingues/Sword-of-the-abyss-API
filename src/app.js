c269191d7c645e3eb1ef92d694dfe8883dbf8b46
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 12:19:36 -0300

    cors-5

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
 
 const index = require('./routes/index')
@@ -6,6 +7,14 @@ const ticketRoute = require('./routes/ticketRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if (req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
+    }
+    next()
+  }))
 app.use('/', index)
 app.use('/ticket', ticketRoute)
 
