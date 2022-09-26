83e0e3ea188664ef3023a9407571aeb954635884
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 20:46:51 -0300

    cors-options

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const cors = require('cors')
 const app = express()
 const index = require('./routes/index')
 const ticketRoute = require('./routes/ticketRoute')
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
+const CorsOptions = {
   origin: '*',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   methods: 'GET, PUT, POST, DELETE, TRUNCATE'
-}))
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors(CorsOptions))
 app.use('/', index)
 app.use('/ticket', ticketRoute)
 
