fe055b4a04dd4cb4fb1cf3477d6159c58ab706ce
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 12:09:41 -0300

    cors-2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,15 @@ const express = require("express")
 const cors = require("cors")
 const router = express.Router()
 
-router.use(cors({
-  Origin: 'https://mauro-domingues.github.io/Sword-of-the-abyss, https://mauro-domingues.github.io/Sword-of-the-abyss-admin',
-  Header: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  Methods: 'GET, PUT, POST, DELETE, TRUNCATE'
-}))
-
 router.get("/", (req, res, next) => {
   try{
+    router.use(cors(() => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+      if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
+      }
+    }))
     res.status(200).send("Conexão com o Banco de Dados estabelecida")
   }catch(error){
     res.status(400).send({
