29db763f40edb11499f8b98859271b1b0a363515
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-26 21:52:25 -0300

    ''

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader("Access-Control-Request-Headers", "*")
+  res.setHeader('Access-Control-Request-Headers', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.setHeader("Access-Control-Allow-Credentials", "true")
-  res.setHeader("Access-Control-Max-Age", "3600")
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Max-Age', '3600')
   if (req.method === 'OPTIONS'){
     res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
   }next()
