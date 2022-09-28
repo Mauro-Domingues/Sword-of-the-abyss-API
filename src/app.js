f63d6a497647af5ef169ac3e8e7bfbd015c48d71
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-09-28 12:30:27 -0300

    origin-updated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const ticketRoute = require('./routes/ticketRoute')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mauro-domingues.github.io/Sword-of-the-abyss-admin/, https://mauro-domingues.github.io/Sword-of-the-abyss/')
+  res.header('Access-Control-Allow-Origin', 'https://mauro-domingues.github.io')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   if (req.method === 'OPTIONS'){
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, TRUNCATE')
