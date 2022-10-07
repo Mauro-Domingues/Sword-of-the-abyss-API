ac644d28fab61e02049396ddec05ca96ad283807
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-10-07 16:27:22 -0300

    auth de login do admin

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 const checkAdmin = require('../middlewares/checkAdmin')
+const checkUser = require('../middlewares/checkUser')
 
 router.get('/', checkAdmin, userController.get)
 router.post('/', checkAdmin, userController.post)
 router.delete('/:id', checkAdmin, userController.delete)
 router.post('/login', userController.checkUser)
-router.post('/login-admin', userController.checkAdmin)
+router.post('/login-admin', checkUser, userController.checkAdmin)
 
 module.exports = router
